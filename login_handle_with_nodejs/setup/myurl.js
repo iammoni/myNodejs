31629cca7d98b44d36249e6384749f973d7df361
iammoni <46134480+iammoni@users.noreply.github.com>
2020-01-02 15:25:27 +0530

    Update myurl.js

--- login_handle_with_nodejs/setup/myurl.js
+++ login_handle_with_nodejs/setup/myurl.js
@@ -1,4 +1,4 @@
 module.exports={
-    url:"mongodb+srv://Shubham1:mla13003773%40ram@cluster0-osjv4.mongodb.net/test?retryWrites=true&w=majority",
-    secret:'HelloBalaji'
+    url:"",
+    secret:''
 };
